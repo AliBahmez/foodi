493679c3d1fafa9879828ed9fa08bcd92354e538
AliBahmez <774514428ali@gmail.com>
2023-10-15 13:32:33 +0300

    update page javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,23 @@
-let spans = document.getElementsByName('select');
+let spans = document.getElementsByClassName('select');
 let box = document.getElementsByClassName('standout-part2')[0];
 
 function move(index){
     let sp = spans[index];
-    box.style.transform = `translateX(-${index * 25.4}%)`;
-       
+    box.style.transform = `translateX(-${index * 25}%)`;
+    // reomveactive();
+    // spans[index].className='color-button'
+    // console.log(spans[index])
+
+    // index == 0 ? spans[index ].classList.add('color-button') : spans[index - 1 ].classList.add('color-button');
 }
+// let active = document.getElementsByClassName('color-button');
+// function reomveactive(){
+// for(let i=0 ; i<2; i++)
+// // for(let i of )
+// {
+// // active[i].className='';
+// active[i].removeChild('color-button')
+// // i.getElementsByTagName('button').className='';
+// // i.document.getElementById('select').className='';
+// }      
+// }}
